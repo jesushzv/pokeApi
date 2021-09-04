d5bd3908b2ac8579d28e3109dd34548eed151ca5
Jesus Hector Zamora Valenzuela <jhector.zamora@hotmail.com>
2021-09-04 09:00:38 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,96 @@
+window.onload = async function () {
+  let url = "https://pokeapi.co/api/v2/pokemon/";
+  for (let i = 1; i <= 151; i++) {
+    let link = `${url}${i}`;
+
+    let pokemon = await fetch(link).then((e) => e.json());
+
+    //Div that will contain the specific pokemon
+    $(".main").append(`<div id='${pokemon.name}'>
+     </div>`);
+
+     
+     //Name
+     $(`div#${pokemon.name}`).append(`<h1 class='toggle ${pokemon.name}'>${pokemon.name}</h1>`);
+     
+     //Image
+     $(`div#${pokemon.name}`).append(`<img src=${pokemon.sprites.front_default}>`);
+     
+     //ID
+     $(`div#${pokemon.name}`).append(`<p> <strong> ID: </strong> ${pokemon.id}</p>`);
+     
+     
+     
+     
+     
+     //Div that will contain the hidden content
+     $(`div#${pokemon.name}`).append(`<div class='hidden ${pokemon.name}'></div>`)
+    
+     //Hide content
+    $(`.hidden.${pokemon.name}`).hide();
+
+
+    //Show content
+    $(`h1.toggle.${pokemon.name}`).click(()=>{
+      $(`.hidden.${pokemon.name}`).toggle();
+    })
+
+    //Abilities
+    $(`.hidden.${pokemon.name}`).append(
+      `<p > <strong> Abilities: </strong></p> <p class='${pokemon.name} abilities'> </p>`
+    );
+
+    pokemon["abilities"].map((e) => {
+      $(`.${pokemon.name}.abilities`).append(` ${e.ability.name}`);
+    });
+
+    //Types
+    $(`.hidden.${pokemon.name}`).append(`<p> <strong> Types: </strong></p>`);
+    pokemon["types"].map((e) => {
+      $(`.hidden.${pokemon.name}`).append(`<p> ${e.type.name} </p>`);
+    });
+
+    //Stats
+    $(`.hidden.${pokemon.name}`).append(`<p><strong>Stats:</strong></p>`);
+    pokemon["stats"].map((e) => {
+      $(`.hidden.${pokemon.name}`).append(
+        `<p> <strong> ${e.stat.name}:</strong> ${e.base_stat} </p>`
+      );
+    });
+
+    //Profile
+    $(`.hidden.${pokemon.name}`).append(
+      `<p> <strong> Weight: </strong> ${pokemon.weight} lb </p>`
+    );
+    $(`.hidden.${pokemon.name}`).append(
+      `<p> <strong> Height: </strong> ${pokemon.height} cm </p>`
+    );
+
+    //Moves
+    $(`.hidden.${pokemon.name}`).append(`<p><strong>Moves:</strong></p>`);
+    pokemon["moves"].map((e) => {
+      $(`.hidden.${pokemon.name}`).append(`<p> ${e.move.name} </p>`);
+    });
+  }
+};
+
+/* $(".get").click(async () => {
+  let url = "https://pokeapi.co/api/v2/pokemon/1";
+  const pokemon = await fetch(url).then((e) => e.json());
+
+  console.log(pokemon)
+
+  $('.main').append(`<p> <strong> Name: </strong> ${pokemon.name}</p>`)
+
+  $('.main').append(`<strong>Types</strong>`)
+
+
+
+  $('.main').append(`<strong>Stats</strong>`)
+
+  pokemon['stats'].map((e)=>{
+    $('.main').append(`<p> <strong> ${e.stat.name}:</strong> ${e.base_stat} </p>`)
+  })
+
+ 
+}); */
